82f030b42323f74c372f5600828f9897a3636c49
Totto16 <tobiausgais@gmail.com>
2026-04-18 02:27:15 +0200

    feat: add some needed apis (just in the header atm)

--- src/tjson_schema.h
+++ src/tjson_schema.h
@@ -28,7 +28,7 @@ TJSON_NODISCARD tstr json_schema_to_string(const JsonSchema* schema);
 TJSON_NODISCARD JsonSchemaObject* json_schema_object_get_empty(void);
 
 TJSON_NODISCARD tstr_static json_schema_object_add_entry(JsonSchemaObject* json_schema_object,
-                                                         tstr** moved_key, JsonSchema value,
+                                                         tstr* moved_key, JsonSchema value,
                                                          bool required);
 
 TJSON_NODISCARD tstr_static json_schema_object_add_entry_dup(JsonSchemaObject* json_schema_object,
@@ -39,9 +39,27 @@ void free_json_schema_object(JsonSchemaObject* json_schema_object);
 
 TJSON_NODISCARD JsonSchemaArray* json_schema_array_get(JsonSchema items);
 
+TJSON_NODISCARD tstr_static json_schema_array_set_min(JsonSchemaArray* json_schema_arr, size_t min);
+
 void free_json_schema_array(JsonSchemaArray* json_schema_arr);
 
 TJSON_NODISCARD JsonSchemaString* json_schema_string_get(void);
+
+TJSON_NODISCARD tstr_static json_schema_string_set_nonempty(JsonSchemaString* json_schema_str);
+
+TJSON_NODISCARD tstr_static json_schema_string_set_min(JsonSchemaString* json_schema_str,
+                                                       size_t min);
+
+typedef struct JsonSchemaRegex* JsonSchemaRegex;
+
+TJSON_NODISCARD JsonSchemaRegex* json_schema_regex_get(const char* str);
+
+TJSON_NODISCARD JsonSchemaRegex* json_schema_regex_get_tstr(const tstr* str);
+
+void free_json_schema_regex(JsonSchemaRegex* json_schema_regex);
+
+TJSON_NODISCARD tstr_static json_schema_string_set_regex(JsonSchemaString* json_schema_str,
+                                                         const JsonSchemaRegex* regex);
 
 void free_json_schema_string(JsonSchemaString* json_schema_string);
 
